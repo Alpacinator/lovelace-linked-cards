0cc2bf297cfa6f992799b248d9d9124c5d675213
Alpacinator <77163848+Alpacinator@users.noreply.github.com>
2026-08-10 19:53:38 +0200

    Update plugin version to 2.6.0.0

--- linked-card.js
+++ linked-card.js
@@ -19,7 +19,7 @@
  */
 
 const CACHE_TTL_MS   = 30_000;
-const PLUGIN_VERSION = '2.5.8';
+const PLUGIN_VERSION = '2.6.0.0';
 
 // ----------------------------------------------------------------- version check --
 // Compares the running version against the last seen version in localStorage.
